faa61382a2b5f596cb5cb864a8a14c3aa3912c6c
Roi Lipman <roi.lipman@avg.com>
2014-02-23 16:01:09 +0200

    Initial commit.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public Calculator()
+        {
+ 
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return Add(a, -b);
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Div(int a, int b)
+        {
+            // May throw division by zero!
+            return Mul(a, 1 / b);
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            int res = calc.Add(calc.Mul(2, 4), calc.Sub(10, 8));
+
+            Console.WriteLine("8 + 2 = " + res);
+        }
+    }
+}
